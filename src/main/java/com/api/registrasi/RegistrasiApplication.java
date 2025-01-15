6e1a6b8304d1de330ff64852dd174a4693790184
M Hamdan Syakirin <900373@HQ.bni.co.id>
2025-01-15 10:21:38 +0700

    New dev api registrasi

---
+++ src/main/java/com/api/registrasi/RegistrasiApplication.java
@@ -0,0 +1,13 @@
+package com.api.registrasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrasiApplication.class, args);
+	}
+
+}
